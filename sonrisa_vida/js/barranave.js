3de82e9cf3adc7057ffe8ffac7f8e4f5f8e46b06
freddy andres delgado cortes <122740674+andress21@users.noreply.github.com>
2023-06-20 21:49:56 -0500

    primer commit

---
+++ sonrisa_vida/js/barranave.js
@@ -0,0 +1,42 @@
+	// Sticky Navbar
+    let header = document.querySelector('header');
+    let menu = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+     
+     
+    window.addEventListener('scroll', () => {
+        header.classList.toggle('shadow', window.scrollY > 0);
+    });
+     
+    menu.onclick = () => {
+        navbar.classList.toggle('active');
+    }
+    window.onscroll = () => {
+        navbar.classList.remove('active');
+    }
+     
+    // Dark Mode
+    let darkmode = document.querySelector('#darkmode');
+     
+    darkmode.onclick = () => {
+        if(darkmode.classList.contains('bx-moon')){
+            darkmode.classList.replace('bx-moon','bx-sun');
+            document.body.classList.add('active');
+        }else{
+            darkmode.classList.replace('bx-sun','bx-moon');
+            document.body.classList.remove('active');
+        }
+    }
+
+    /*
+    const persona ={
+        nombre:"freddy",
+        apellido: "cortes" 
+    }
+
+    function cambiarvalor(p1){
+        p1.nombre="caliche";
+        p1.apellido="larote";
+    }
+    cambiarvalor(persona);
+    console.log(persona);*/
